9b92148fed73f5e7b4015e352a54b48b3592c00d
IsaacMPereira <isaacp.menezesp@gmail.com>
2026-02-11 12:19:10 -0300

    Avanços na matriz de equivalência. Faltam ajustes no assigning correto

--- elipticas/matrizClasseEquivalencia.c
+++ elipticas/matrizClasseEquivalencia.c
@@ -21,23 +21,45 @@ int main(int argc, char *argv[]){
 		printf("Number of columns must be odd\n");
 		exit(1);
 	}
-	//int classes[mod-1][col];
+	int classes[mod][col];
 
 	//Finding the first element in the matrix
 	int aux = floor(col/2);
-	int first = 0; //Element in the 00 position in the matrix
-	for(int i=0; i<aux; i++){
+	int first = 0; //Element in the 00-th position in the matrix
+	for(int i=0; i<aux; i++)
 		first -= mod;
-	}
-	printf("\n%d", first);
 	
+	int count = first; //This will help the correct assigning
+
 	//Finding the last element in the matrix
-	int last = mod-1; //Element in the ij position in the matrix
-	for(int i=0; i<aux; i++){
+	int last = mod-1; //Element in the ij-th position in the matrix
+	for(int i=0; i<aux; i++)
 		last += mod;
+	
+	//Assigning
+	for(int i=0; i<mod; i++){
+		for(int j=0; j<col; j++){
+			if(i == 0 && j == 0){
+				printf("%d\n", first);
+				classes[i][j] = first;
+			}else{
+				first += 1;
+				printf("%d\n", first);
+				classes[i][j] = first;
+			}
+			if(first == last) break;
+		}
 	}
-	printf("\n%d\n", last);
 
+	//Printing
+	printf("\n\n");
+	for(int i=0; i<mod; i++){
+		for(int j=0; j<col; j++){
+			printf("%d\t", classes[i][j]);	
+		}
+		printf("\n");
+	}
 
+	printf("\n");
 	return 0;
 }
